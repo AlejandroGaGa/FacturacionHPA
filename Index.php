d2bcb895982123a8312355dd73b960b196785291
AlejandroGaGa <alejandrogarciagallardo1234@gmail.com>
2023-04-27 10:11:00 -0600

    Changes on design

--- Index.php
+++ Index.php
@@ -1,71 +1,73 @@
 <?php
-$alert='';
+$alert = '';
 session_start();
-if(!empty($_SESSION['active'])){
+if (!empty($_SESSION['active'])) {
     header('location: sistema/');
-}else{
-if(!empty($_POST))
-{
-if(empty($_POST['usuario']) || empty($_POST['clave'])){
-    $alert='Ingrese su usuario y la clave';
-}
-else
-{
-require_once "conexion.php";
-$user = mysqli_real_escape_string($conection,$_POST['usuario']);
-$pass = md5(mysqli_real_escape_string($conection,$_POST['clave']));
+} else {
+    if (!empty($_POST)) {
+        if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+            $alert = 'Ingrese su usuario y la clave';
+        } else {
+            require_once "conexion.php";
+            $user = mysqli_real_escape_string($conection, $_POST['usuario']);
+            $pass = md5(mysqli_real_escape_string($conection, $_POST['clave']));
 
-$query = mysqli_query($conection,"SELECT *from usuario WHERE usuario = '$user' AND clave = '$pass'");
-mysqli_close($conection);
-$result=mysqli_num_rows($query);
+            $query = mysqli_query($conection, "SELECT *from usuario WHERE usuario = '$user' AND clave = '$pass'");
+            mysqli_close($conection);
+            $result = mysqli_num_rows($query);
 
-if($result>0)
-{
-$data =  mysqli_fetch_array($query);
+            if ($result == 0) {
+                $data = mysqli_fetch_array($query);
 
-$_SESSION['active'] = true;
-$_SESSION['idUser'] = $data['idusuario'];
-$_SESSION['nombre'] = $data['nombre'];
-$_SESSION['email'] = $data['correo'];
-$_SESSION['user'] = $data['usuario'];
-$_SESSION['rol'] = $data['rol'];
-header('location: sistema/');
-}
-else{
-    $alert='El usuario con la clave es incorrecto';
- session_destroy();
+                $_SESSION['active'] = true;
+                $_SESSION['idUser'] = $data['idusuario'];
+                $_SESSION['nombre'] = $data['nombre'];
+                $_SESSION['email'] = $data['correo'];
+                $_SESSION['user'] = $data['usuario'];
+                $_SESSION['rol'] = $data['rol'];
+                header('location: sistema/');
+            } else {
+                $alert = 'El usuario con la clave es incorrecto';
+                session_destroy();
+            }
+        }
     }
-  } 
-}
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesión | HPA</title>
-<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
-    <section id="container"> 
+    <section id="container">
 
-    <form action="" method="post">
-    <h3>INICIO DE SESIÓN</h3>
+        <form action="" method="post">
+            <h3>INICIO DE SESIÓN</h3>
 
-    <img src="img/HPAtrans.png" alt ="Login" ></img>
-<center><p>System Billing</p></center>
-    <input type ="text" name="usuario" placeholder="Usuario">
-    <input type="password" name="clave" placeholder="Contraseña">
-<div class="alert"><?php echo isset($alert) ? $alert : '';?></div>
-    <input type=submit value="Iniciar Sesión">
-    
-</form>
+            <img src="img/HPAtrans.png" alt="Login"></img>
+            <center>
+                <p>Herrajes para aluminio S.A de C.V.</p>
+            </center>
+            <input type="text" name="usuario" placeholder="Usuario">
+            <input type="password" name="clave" placeholder="Contraseña">
+            <div class="alert">
+                <?php echo isset($alert) ? $alert : ''; ?>
+            </div>
+            <input type=submit value="Iniciar Sesión">
+
+        </form>
 
     </section>
-<footer class="footer" >
-    <p>Desing for Alejandro García Gallardo.</p>
-    <p>Todos los derechos reservados | Copyrigth 2020.</p>
-</footer>
+    <footer class="footer">
+        <p>Desing for Alejandro García Gallardo.</p>
+        <p>Todos los derechos reservados | Copyrigth 2020.</p>
+    </footer>
 </body>
+
 </html>
--- sistema/registro_proveedor.php
+++ sistema/registro_proveedor.php
@@ -2,46 +2,40 @@
 
 session_start();
 
-if($_SESSION['rol'] != 1 and $_SESSION['rol'] != 2)
-{
+if ($_SESSION['rol'] === 3) {
   header('location: ../');
 }
 
 include "../conexion.php";
 
-if(!empty($_POST))
-{
-$alert='';
-if(empty($_POST['proveedor']) || empty($_POST['contacto']) || empty($_POST['telefono']) || empty($_POST['direccion']))
- {
- $alert = '<p class="msg_error">Todos los campos son obligatorios.</p>';
- }// fin de if empty
- else
- {
-   
-   $proveedor        = $_POST['proveedor'];
-   $contacto     = $_POST['contacto'];
-   $telefono   = $_POST['telefono'];
-   $direccion  = $_POST['direccion']; 
-   $usuario_id = $_SESSION['idUser'];
+if (!empty($_POST)) {
+  $alert = '';
+  if (empty($_POST['proveedor']) || empty($_POST['contacto']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
+    $alert = '<p class="msg_error">Todos los campos son obligatorios.</p>';
+  } // fin de if empty
+  else {
 
-   
- $query_insert = mysqli_query($conection,"INSERT INTO proveedor(proveedor,contacto,telefono,direccion,usuario_id)
+    $proveedor = $_POST['proveedor'];
+    $contacto = $_POST['contacto'];
+    $telefono = $_POST['telefono'];
+    $direccion = $_POST['direccion'];
+    $usuario_id = $_SESSION['idUser'];
+
+
+    $query_insert = mysqli_query($conection, "INSERT INTO proveedor(proveedor,contacto,telefono,direccion,usuario_id)
                                              VALUES('$proveedor','$contacto','$telefono','$direccion','$usuario_id')");
 
- if($query_insert)
-    {
-// si es verdadero si se inserto o eso creo 0.o :(
-    $alert = '<p class="msg_save">Proveedor creado correctamente.</p>';
-     }// fin de if query_insert
-     else
-     {
-        $alert = '<p class="msg_error">Error al crear el proveedor.</p>';
+    if ($query_insert) {
+      // si es verdadero si se inserto o eso creo 0.o :(
+      $alert = '<p class="msg_save">Proveedor creado correctamente.</p>';
+    } // fin de if query_insert
+    else {
+      $alert = '<p class="msg_error">Error al crear el proveedor.</p>';
 
-      }// fin de else  en donde si el NIP ya existe entonces si se insertara la informacion 
-   }// fin de else de empty
-   mysqli_close($conection);
- }
+    } // fin de else  en donde si el NIP ya existe entonces si se insertara la informacion 
+  } // fin de else de empty
+  mysqli_close($conection);
+}
 
 
 
@@ -49,50 +43,56 @@ if(empty($_POST['proveedor']) || empty($_POST['contacto']) || empty($_POST['tele
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <?php 
+  <?php
   include "includes/scripts.php";
   ?>
-    
+
   <title>HPA | Registro proveedor</title>
-  
+
 </head>
+
 <body>
-  <?php 
+  <?php
   include "includes/nav.php"
-  ?><br><br>
+    ?><br><br>
   <br>
   <br>
   <br>
   <section id="container">
     <div class="form_register">
-        <h1><i class="far fa-bookmark"></i> Registro de proveedor</h1>
-        <hr>
-        <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
-  
+      <h1><i class="far fa-bookmark"></i> Registro de proveedor</h1>
+      <hr>
+      <div class="alert">
+        <?php echo isset($alert) ? $alert : ''; ?>
+      </div>
 
-        <form action="" method="post">
+
+      <form action="" method="post">
         <label for="proveedor">Proveedor</label>
-        <input type ="text" name="proveedor" id="proveedor" placeholder="Nombre del Proveedor"></input>
+        <input type="text" name="proveedor" id="proveedor" placeholder="Nombre del Proveedor"></input>
         <label for="contacto">Contacto</label>
-        <input type ="text" name="contacto" id="contacto" placeholder="Nombre completo contacto.(obligatorios)"></input>
+        <input type="text" name="contacto" id="contacto" placeholder="Nombre completo contacto.(obligatorios)"></input>
         <label for="usuario">Telefono</label>
-        <input type ="number" name="telefono" id="telefono" placeholder="Telefono local.(obligatorios)"></input>
-       <label for="clave">Dirección</label>
-        <input type ="text" name="direccion" id="direccion" placeholder="Dirección completa.(obligatorios)"></input>
-     <center> <button type="submit" class="btn_save"><i class="fas fa-user-plus"></i> Crear proveedor</button></center>
-    <!-- <center> <a type="submit"  class="btn_save" href="lista_cliente.php">Regresar a lista de clientes </a></center>-->
-       </form>
-        </div>
-      
-      
-       
-        
+        <input type="number" name="telefono" id="telefono" placeholder="Telefono local.(obligatorios)"></input>
+        <label for="clave">Dirección</label>
+        <input type="text" name="direccion" id="direccion" placeholder="Dirección completa.(obligatorios)"></input>
+        <center> <button type="submit" class="btn_save"><i class="fas fa-user-plus"></i> Crear proveedor</button>
+        </center>
+        <!-- <center> <a type="submit"  class="btn_save" href="lista_cliente.php">Regresar a lista de clientes </a></center>-->
+      </form>
+    </div>
+
+
+
+
   </section>
 
-  <?php 
+  <?php
   include "includes/footer.php"
-  ?>
+    ?>
 </body>
+
 </html>
